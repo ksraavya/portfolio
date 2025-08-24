0e5334aff5b49cb406d9abf56587f97d8d243f6b
ksraavya <ksraavya05@gmail.com>
2025-08-24 16:37:05 +0530

    initial commit

---
+++ src/app/data.js
@@ -0,0 +1,102 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "AskData",
+    description: "Query your data",
+    date: "2022-08-15",
+    demoLink: "https://askdata-theta.vercel.app/",
+  },
+  {
+    id: 2,
+    name: "Pathshala",
+    description: "AR/VR EdTech platform",
+    date: "2022-06-20",
+    demoLink: "https://github.com/ksraavya/pathshala",
+  },
+  {
+    id: 3,
+    name: "Inkling",
+    description: "Blog website",
+    date: "2022-09-10",
+    demoLink: "https://github.com/ksraavya/blog",
+  },
+  {
+    id: 4,
+    name: "Taskly",
+    description: "Task manager",
+    date: "2022-05-30",
+    demoLink: "https://github.com/ksraavya/task-manager",
+  },
+  {
+    id: 5,
+    name: "Civis",
+    description: "Know your benefits and rights",
+    date: "2022-07-12",
+    demoLink: "https://github.com/ksraavya/civis",
+  },
+  {
+    id: 6,
+    name: "Startup Sphere",
+    description: "Website for college society",
+    date: "2022-10-01",
+    demoLink: "https://github.com/ksraavya/ssphere",
+  },
+  {
+    id: 7,
+    name: "Klimate",
+    description: "Weather app",
+    date: "2022-04-18",
+    demoLink: "https://github.com/ksraavya/climate-app",
+  },  
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://github.com/ksraavya",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/sraavya-kochhar-b97367291/",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "X",
+    link: "#",
+    icon: "twitter",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "/SRAAVYA_KOCHHAR_resume1.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
